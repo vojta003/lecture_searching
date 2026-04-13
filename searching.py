83a586972b7f74deadd5652781dc5c502de6a1a6
vojta003 <luky.neuzil@gmail.com>
2026-04-13 13:15:57 +0200

    Uprava search

--- searching.py
+++ searching.py
@@ -1,10 +1,13 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
 
 
 def read_data(file_name, field):
+    data = {}
+
     """
     Reads json file and returns sequential data.
     :param file_name: (str), name of json file
@@ -12,11 +15,17 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
-
+    if not os.path.join(cwd_path, file_name):
+        return None
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    if field in data:
+        return data
+    return data[field]
 
 def main():
-    pass
-
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
 if __name__ == '__main__':
     main()
